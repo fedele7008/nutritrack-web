b296a96db157f5a56e75935cfbbb6b2fa55507c1
Stephanie Xu <stephxca@gmail.com>
2023-07-06 00:09:06 -0400

    update global styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.js";
 import Restaurants from "./pages/Restaurants.js";
 import FoodLogs from "./pages/FoodLogs.js";
+import { ThemeProvider, createTheme } from '@mui/material';
 
 const App = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Segoe UI, Roboto, sans-serif', // Set your desired font family here
+    },
+  });
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/restaurants" element={<Restaurants />} />
         <Route path="/FoodLogs" element={<FoodLogs />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
