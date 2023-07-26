9c81e015e4dc67ef93ae010a49717a038c54fd27
DESKTOP-HOC4NOO\fedel <fedelejohn7008@gmail.com>
2023-07-26 10:32:25 -0400

    size change

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -11,15 +11,13 @@ function Statistics() {
   return (
     <Stack direction="column" alignItems="center" justifyContent="center" marginTop={3} className="contentBox">
       <Typography variant="h4" sx={{ fontWeight: '600' }}>Statistics</Typography>
-      <Grid container spacing={1} rowSpacing={1} width={"75%"} marginTop={1}>
+      <Grid container spacing={1} rowSpacing={1} width={"90%"} marginTop={1}>
         <Grid item xs={5}>
           <LoggedFoodStatPie/>
         </Grid>
         <Grid item xs={7}>
           <AvgCaloriesStatBar/>
         </Grid>
-      </Grid>
-      <Grid container spacing={1} rowSpacing={1} width={"75%"} marginTop={1}>
         <Grid item xs={8}>
           <RestaurantTrendStat/>
         </Grid>
